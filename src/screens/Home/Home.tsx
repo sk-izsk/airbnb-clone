8da2c017757931a4cfa4fcfb3542c5ca737b4bdb
Zeeshan <sk.zeeshan1992@gmail.com>
2020-09-16 21:25:34 -0400

    add header

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,16 @@
-import { Typography } from '@material-ui/core';
+import { Box, makeStyles } from '@material-ui/core';
 import React from 'react';
+import { CustomTheme } from '../../theme/muiTheme';
 
 export interface HomeProps {}
 
+const useStyles = makeStyles((theme: CustomTheme) => ({
+  home: {},
+}));
+
 const Home: React.FC<HomeProps> = () => {
-  return <Typography variant='h3'>this is home</Typography>;
+  const classes = useStyles();
+  return <Box className={classes.home}>this is home</Box>;
 };
 
 export default Home;
